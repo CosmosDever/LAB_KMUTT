713381565703b5bd7cc036f08b7c3fd45e4ecd12
CosmosDever <46952477+CosmosDever@users.noreply.github.com>
2022-10-28 10:23:26 +0700

    Create Prob1_3.py

---
+++ EX2/Prob1_3.py
@@ -0,0 +1,6 @@
+def p13():
+    student_names = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    scores = [9, 22, 60, 58, 70, 12, 33, 99, 57, 82]
+    grade_a_student_with_score = [(student_names[i],scores[i]) for i in range(len(scores)) if scores[i]>=80]
+    #Expected Ans -> grade_a_student_with_score = [('h', 99), ('j', 82)]
+    return  grade_a_student_with_score
