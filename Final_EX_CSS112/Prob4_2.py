5de8b8d33f3ac48a4a06ac242ae0255e3dd3abe9
CosmosDever <46952477+CosmosDever@users.noreply.github.com>
2022-12-15 12:03:28 +0700

    Update Prob4_2.py

--- Final_EX_CSS112/Prob4_2.py
+++ Final_EX_CSS112/Prob4_2.py
@@ -1,4 +1,4 @@
 import numpy as np
-#Problem 4.2 
+
 def Problem4_2(A):
     return [A[i][j] for i in range(len(A)) for j in range(len(A[i])) if i % 2 == 0 and j % 2 == 0 or i % 2 == 1 and j % 2 == 1]
