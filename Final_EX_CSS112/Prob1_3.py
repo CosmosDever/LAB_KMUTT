14652bfa17a35deeee59315fa8cb067ecdbad22c
CosmosDever <46952477+CosmosDever@users.noreply.github.com>
2022-12-15 10:34:08 +0700

    Update Prob1_3.py

--- Final_EX_CSS112/Prob1_3.py
+++ Final_EX_CSS112/Prob1_3.py
@@ -2,4 +2,3 @@
 def Problem1_3(purchase_list):
     icecream_price_dict = {'cone':'300', 'sherbet':'200', 'choco': '100'}
     return sum(int(icecream_price_dict[item]) if item in icecream_price_dict else 0 for item in purchase_list)
-print(Problem1_3(['boba','boba','cone','choco','choco']))
