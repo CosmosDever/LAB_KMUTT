5acb1020ae0d83e5ebf6f5e187751ba5b7e15a6b
CosmosDever <46952477+CosmosDever@users.noreply.github.com>
2022-12-15 11:05:55 +0700

    Update Prob4_3.py

--- Final_EX_CSS112/Prob4_3.py
+++ Final_EX_CSS112/Prob4_3.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-def Problem4_3(A): #Return inverse of A by manual calculation
-    #Your code here
-    return #Your code here
+def Problem4_3(A): 
+    det = np.linalg.det(A)
+    a, b, c, d = A.flatten()
+    A_inv = 1/det * np.array([[d, -b], [-c, a]])
+    return A_inv
